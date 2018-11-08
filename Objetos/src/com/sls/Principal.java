412c08567b21045cdf62d8d69b42b9138bfd4d2c
Rebel1898 <Sergio.Llamas@protonmail.ch>
2018-11-08 12:51:22 +0100

    'message'

---
+++ Objetos/src/com/sls/Principal.java
@@ -0,0 +1,24 @@
+package com.sls;
+import com.sls.models.Usuario;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		Usuario pepito =new Usuario();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
